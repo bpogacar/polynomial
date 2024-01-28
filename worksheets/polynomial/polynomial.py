8e90152568cf2fae35440b26aeee937336e73c61
Benjamin Pogacar <bpogacar@bu.edu>
2024-01-28 16:13:26 -0500

    cs 506 exercise part c

--- worksheets/polynomial/polynomial.py
+++ worksheets/polynomial/polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,13 +26,27 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
+    def evaluate(self, x):
+        assert(isinstance(self.p1, (Int, X, Add, Sub, Mul, Div)) and isinstance(self.p2, (Int, X, Add, Sub, Mul, Div)))
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
+        if isinstance(self.p1, (Add,Div,Mul)):
+            if isinstance(self.p2, (Add,Div,Mul)):
+                 return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) - " + repr(self.p2)
+        if isinstance(self.p2, (Add,Div,Mul)):
+            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, x):
+        assert(isinstance(self.p1, (Int, X, Add, Sub, Mul, Div)) and isinstance(self.p2, (Int, X, Add, Sub, Mul, Div)))
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
     
 class Div:
     def __init__(self, p1, p2):
@@ -42,6 +62,10 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, x):
+        assert(isinstance(self.p1, (Int, X, Add, Sub, Mul, Div)) and isinstance(self.p2, (Int, X, Add, Sub, Mul, Div)))
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -55,7 +79,14 @@ class Mul:
         if isinstance(self.p2, (Add,Sub,Div)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        assert(isinstance(self.p1, (Int, X, Add, Sub, Mul, Div)) and isinstance(self.p2, (Int, X, Add, Sub, Mul, Div)))
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 
 
-poly = Sub( Add( Int(4), Int(3)), Add( X(), Div( Int(1), Sub( Mul(X(), X()), Int(1)))))
+
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Div( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(-1))
